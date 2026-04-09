4f206cd7746b46e4870d99a19c9c2270867a8839
28111987 <164403080+28111987@users.noreply.github.com>
2026-04-09 21:59:50 +0530

    Restore canonical server.js v1.1.3

--- server.js
+++ server.js
@@ -381,7 +381,7 @@ app.use(cors({
   credentials: false,
 }));
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "8mb" }));
 
 app.use((req, res, next) => {
   req.id = `req_${Date.now().toString(36)}_${Math.random().toString(36).slice(2, 8)}`;
